86079ceea9046241e3d238db543e2fc57ae6c7a2
yanyue <yanyue@actionsky.com>
2022-10-17 13:08:55 +0800

    递归案例

--- mathClass/math1.go
+++ mathClass/math1.go
@@ -1,4 +1,18 @@
 package mathClass
-func Add(x,y int) int {
-    return x + y
+
+func Add(x, y int) int {
+	return x + y
+}
+func JieChen(num int) (result int) {
+	if num > 0 {
+		result = num * JieChen((num - 1))
+		return result
+	}
+	return 1
+}
+func Fibonacci(num int) int {
+	if num < 2 {
+		return num
+	}
+	return Fibonacci(num-2) + Fibonacci(num-1)
 }
